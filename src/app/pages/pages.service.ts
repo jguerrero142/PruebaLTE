7392912e362857982a11b961ff782358c8aad798
Julian Guerrero <71576846+jguerrero142@users.noreply.github.com>
2021-11-10 08:05:33 -0500

    feat

--- src/app/pages/pages.service.ts
+++ src/app/pages/pages.service.ts
@@ -7,53 +7,50 @@ import { User } from './class/user.model';
 import { BehaviorSubject } from 'rxjs';
 import { Taks } from './class/taks.model';
 
-
-
-
 @Injectable({
   providedIn: 'root',
 })
 export class PagesService {
-  
-  
   public users: User[] = [];
   public taks: Taks[] = [];
-  private userObservable: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(this.users);
-  private taksObservable: BehaviorSubject<Taks[]> = new BehaviorSubject<Taks[]>(this.taks);
+  private userObservable: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(
+    this.users
+  );
+  private taksObservable: BehaviorSubject<Taks[]> = new BehaviorSubject<Taks[]>(
+    this.taks
+  );
 
-  get getUser(){
+  get getUser() {
     return this.userObservable.asObservable();
-  } 
+  }
   set setUser(users: User[]) {
     this.userObservable.next(users);
   }
 
-  get getTaks(){
+  get getTaks() {
     return this.taksObservable.asObservable();
-  } 
+  }
   set setTaks(taks: Taks) {
-    this.taks.push(taks);    
+    this.taks.push(taks);
     this.taksObservable.next(this.taks);
   }
 
-  set delTaks(i: number){
-    this.taks.splice(i,1);
+  set delTaks(i: number) {
+    this.taks.splice(i, 1);
     this.taksObservable.next(this.taks);
   }
 
   private API_URI = environment.wsUrl;
 
-  constructor(
-    private http: HttpClient
-  ) { 
-      this.getUsers();
+  constructor(private http: HttpClient) {
+    this.getUsers();
   }
 
   // CRUD user.
   //Obetenemos todos los usuarios.
   getUsers() {
-    return this.http.get<User[]>(`${this.API_URI}/usuario`)
-    .subscribe( (d) => this.setUser = d)
+    return this.http.get<User[]>(`${this.API_URI}/usuario`).subscribe((d) => {
+      (this.setUser = d), console.log(d);
+    });
   }
-
 }
