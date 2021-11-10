7eede04152e2a5b21609ebe3a57956173a2f120d
jguerrero142 <julgueover.05@gmail.com>
2021-11-10 01:07:45 -0500

    pruebas

--- src/app/pages/welcome/usuario.component.ts
+++ src/app/pages/welcome/usuario.component.ts
@@ -16,28 +16,7 @@ interface Person {
 })
 export class UsuarioComponent implements OnInit {
 
-  public listOfData: Person[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park'
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park'
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park'
-    }
-  ];
-
-  
+   
   public users: User[] = [];
 
   constructor( private userService: PagesService) { }
